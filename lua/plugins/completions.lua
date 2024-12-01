3f88fbcb3f9e0406b5985b67a0846ecd6e1571bd
Flamur-Maliqi <flamur.maliqi@ibm.com>
2024-12-01 17:53:53 +0100

    final nvim config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,12 +1,14 @@
 return {
   {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
     "L3MON4D3/LuaSnip",
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
-    }
+    },
   },
-
   {
     "hrsh7th/nvim-cmp",
     config = function()
@@ -15,9 +17,8 @@ return {
 
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
@@ -29,11 +30,11 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          -- { name = "nvim_lsp" },
-          { name = "vsnip" }, -- For vsnip users.
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
         }),
