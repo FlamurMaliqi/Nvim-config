621dacbae046ee55cbd4370c5f00da5f7267e6d0
Flamur-Maliqi <flamur.maliqi@ibm.com>
2024-12-01 17:43:46 +0100

    Inital commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+-- Lazy.nvim Setup (Plugin-Manager)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local clone_cmd = { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+  local output = vim.fn.system(clone_cmd)
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { output, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("vim-options")
+require("lazy").setup("plugins")
+
+vim.opt.mouse = "a"
+
+
